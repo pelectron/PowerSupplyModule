8c40d5a416929895cecfc44acd18745845d5ee78
pelectron <43130018+pelectron@users.noreply.github.com>
2025-05-12 22:31:41 +0200

    ads5293 first version complete

--- software/include/drivers/ad5293.hpp
+++ software/include/drivers/ad5293.hpp
@@ -1,3 +1,9 @@
+/**
+ * @file drivers/ad5293.hpp
+ * @brief This file contains the driver for the AD5293 digital potentiometer
+ * from Analog Devices.
+ */
+
 #ifndef AD5293_HPP
 #define AD5293_HPP
 
@@ -6,49 +12,112 @@
 #include <cstdint>
 namespace ad5293 {
 
-enum class Mode { performance = 0, normal = 1 };
+/// mode enumeration
+enum class Mode {
+  performance = 0, //< performance mode (default)
+  normal = 1       //< normal mode
+};
 
+/**
+ * @class AD5293
+ * @brief This is a driver for the AD5293 10 bit digital potentiometer from
+ * Analog Devices.
+ * It uses a hal::spi::Device. It assumes that the RESET pin is permanently
+ * pulled high.
+ *
+ * All operations that can fail because of io return a
+ * hal::Error value.
+ */
 class AD5293 {
 public:
-  constexpr hal::Error enable() {
-    control &= ~1u;
-    control &= 0b111;
-    return write(SOFT_POWER_DOWN);
+  /**
+   * @brief this will create an invalid driver. One of the init() overloads must
+   * be called before using this.
+   */
+  constexpr AD5293() = default;
+
+  /**
+   * @brief create a device driver with an spi device. spi must be valid.
+   *
+   * @param spi the spi device to use for communication.
+   */
+  constexpr AD5293(hal::spi::Device spi) : spi_(std::move(spi)) {}
+
+  /**
+   * @brief create a device driver with an spi device. spi must be valid.
+   *
+   * @param spi the spi device to use for communication.
+   */
+  constexpr hal::Error init(hal::spi::Device spi) {
+    spi_ = std::move(spi);
+    if (not spi_.is_valid())
+      return hal::Error::invalid_handle;
+
+    control_ = 0;
+    code_ = 512;
+    return disable();
   }
 
+  /// @brief enable the device. This will also release the write protection of
+  /// the device.
+  constexpr hal::Error enable() {
+    control_ &= ~1u;
+    control_ &= 0b111;
+    hal::Error error = write(SOFT_POWER_DOWN);
+    if (error != hal::Error::none)
+      return error;
+    return write_protection(false);
+  }
+
+  /// @brief disable the device
   constexpr hal::Error disable() {
-    control |= 1;
-    control &= 0b111;
+    control_ |= 1;
+    control_ &= 0b111;
     return write(SOFT_POWER_DOWN | 1);
   }
 
-  constexpr hal::Error reset() { return write(RESET); }
+  /// @brief reset the device. This will set the wiper position to mid scale.
+  constexpr hal::Error reset() {
+    code_ = 512;
+    return write(RESET);
+  }
 
+  /// @brief set the mode of the device to performance (default) or normal.
   constexpr hal::Error mode(Mode mode) {
     switch (mode) {
     case Mode::performance:
-      control &= ~(1u << 2);
+      control_ &= ~(1u << 2);
       break;
     case Mode::normal:
-      control |= 1u << 2;
+      control_ |= 1u << 2;
       break;
     default:
       return hal::Error::invalid_param;
     }
-    control &= 0b111;
-    return write(WRITE_CONTROL | control);
+    control_ &= 0b111;
+    return write(WRITE_CONTROL | control_);
   }
 
+  /**
+   * @brief enable or disable software write protection. If enabled, the wiper
+   * position cannot be changed until the protection is disabled.
+   */
   constexpr hal::Error write_protection(bool enable) {
     if (enable)
-      control |= 1 << 1;
+      control_ &= ~(1 << 1);
     else {
-      control &= ~(1 << 1);
+      control_ |= 1 << 1;
     }
-    control &= 0b111;
-    return write(WRITE_CONTROL | control);
+    control_ &= 0b111;
+    return write(WRITE_CONTROL | control_);
   }
 
+  /**
+   * @brief set the wiper position to code.
+   *
+   * R_wb = code / 1024 * R_ab
+   * R_wa = (1024 - code) * R_ab
+   */
   constexpr hal::Error code(uint16_t code) {
     code &= code_mask;
     code |= WRITE_RDAC;
@@ -58,24 +127,28 @@ public:
     return hal::Error::none;
   }
 
-  constexpr bool is_enabled() const { return (control & 1u) == 0; }
+  /// returns true if the device is enabled
+  constexpr bool is_enabled() const { return (control_ & 1u) == 0; }
 
+  /// returns the mode of the device
   constexpr Mode mode() const {
-    return (control >> 2) == 1 ? Mode::normal : Mode::performance;
+    return (control_ >> 2) == 1 ? Mode::normal : Mode::performance;
   }
 
-  constexpr bool write_protection() const { return (control & (1 << 1)) != 0; }
+  /// returns true if the device is write protected
+  constexpr bool write_protection() const { return (control_ & (1 << 1)) == 0; }
 
+  /// returns the current wiper position
   constexpr uint16_t code() const { return code_; }
 
 private:
   hal::Error write(uint16_t value) {
-    uint8_t buf[2]{static_cast<uint8_t>(value >> 8),
-                   static_cast<uint8_t>(value)};
-    return spi.write(buf);
+    std::uint8_t buf[2]{static_cast<std::uint8_t>(value >> 8),
+                        static_cast<std::uint8_t>(value)};
+    return spi_.write(buf);
   }
 
-  enum Cmd : uint16_t {
+  enum Cmd : std::uint16_t {
     NOP = 0,
     WRITE_RDAC = 1 << 10,
     READ_RDAC = 1 << 11,
@@ -87,9 +160,9 @@ private:
 
   enum { code_mask = 0b1111111111u };
 
-  hal::spi::Device spi;
-  uint8_t control = 0;
-  uint16_t code_ = 0;
+  hal::spi::Device spi_;
+  uint8_t control_ = 0;
+  uint16_t code_ = 512;
 };
 } // namespace ad5293
 #endif
